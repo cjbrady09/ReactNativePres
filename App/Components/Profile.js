a9caff50e1ac28504206b5522299e395516b58a2
Clayton Brady <cjbrady09@gmail.com>
2018-03-13 09:43:06 -0500

    components

--- App/Components/Profile.js
+++ App/Components/Profile.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { View, Text } from 'react-native'
-import PrimaryButton from '../Components/PrimaryButton'
+import { View, Text, Image} from 'react-native'
 
 // Styles
 import Styles from './Styles/ProfileStyles'
 
 export default class ProfileScreen extends Component {
-  static propTypes = {
-    name: PropTypes.string,
-    changeName: PropTypes.func
-  }
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      name: props.name
-    }
-  }
 
   render () {
     return (
       <View>
-        <PrimaryButton onPress={() => this.props.changeName('Test2')} text={'SUBMIT'} />
-        <Text>{this.props.name}</Text>
+        <Image source={require('../Images/clayton2.jpg')} style={Styles.image} />
+        <Text>Clayton</Text>
+        <Text>
+          Shift <Text style={Styles.blueText}>Interactive</Text>
+        </Text>
+        <Text>Skills</Text>
+        <View />
       </View>
     )
   }
---
+++ App/Components/Styles/ProfileStyles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+import { Fonts, Colors, Metrics } from '../../Themes/'
+
+export default StyleSheet.create({
+  image: {
+    height: 200,
+    width: 150
+  },
+  blueText: {
+    color: '#00f'
+  }
+})
