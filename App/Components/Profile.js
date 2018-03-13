2b7a10af5cd267ad90314bfb3b0861d478402c37
Clayton Brady <cjbrady09@gmail.com>
2018-03-13 10:22:46 -0500

    styled

--- App/Components/Profile.js
+++ App/Components/Profile.js
@@ -8,25 +8,25 @@ export default class ProfileScreen extends Component {
 
   render () {
     return (
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <View style={{flex: 4, flexDirection: 'row'}}>
+      <View style={Styles.mainContainer}>
+        <View style={Styles.profileSection}>
           <View style={Styles.container}>
             <Image source={require('../Images/clayton2.jpg')} style={Styles.canvas} />
           </View>
-          <View style={{flex: 1, flexDirection: 'column'}}>
-            <View style={{flex: 1, justifyContent: 'flex-end'}}>
+          <View style={Styles.infoContainer}>
+            <View style={Styles.nameContainer}>
               <Text>Clayton</Text>
             </View>
-            <View style={{flex: 1, justifyContent: 'flex-start'}}>
+            <View style={Styles.companyContainer}>
               <Text>
                 Shift <Text style={Styles.blueText}>Interactive</Text>
               </Text>
             </View>
           </View>
         </View>
-        <View style={{flex: 8, flexDirection: 'column'}}>
-          <Text style={{flex: 1, alignSelf: 'center', marginTop: 25}}>Skills</Text>
-          <View style={{flex: 7, backgroundColor: '#f00'}} />
+        <View style={Styles.skillsSection}>
+          <Text style={Styles.skills}>Skills</Text>
+          <View style={Styles.tableHolder} />
         </View>
       </View>
     )
--- App/Components/Styles/ProfileStyles.js
+++ App/Components/Styles/ProfileStyles.js
@@ -2,9 +2,37 @@ import { StyleSheet } from 'react-native'
 import { Fonts, Colors, Metrics } from '../../Themes/'
 
 export default StyleSheet.create({
-  image: {
-    height: 200,
-    width: 150
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  profileSection: {
+    flex: 4,
+    flexDirection: 'row'
+  },
+  skillsSection: {
+    flex: 8,
+    flexDirection: 'column'
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  nameContainer: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  companyContainer: {
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  skills: {
+    flex: 1,
+    alignSelf: 'center',
+    marginTop: 25
+  },
+  tableHolder: {flex: 7,
+    backgroundColor: '#f00'
   },
   blueText: {
     color: '#00f'
